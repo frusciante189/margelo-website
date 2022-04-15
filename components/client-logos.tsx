32c7ded5b6cb7a2c6b7ad793cfe9a32cb70e5f0e
Frus <mcagdasbayirli@gmail.com>
2022-04-15 22:31:23 +0300

    slider done i guess

---
+++ components/client-logos.tsx
@@ -0,0 +1,259 @@
+import React from 'react'
+import Image from 'next/image'
+import SliderContainer, { SliderItem } from './slider'
+
+const ClientLogos: React.FC = () => {
+  return (
+    <>
+      <SliderContainer className="" contentWidth={1290} initialOffsetX={0}>
+        <SliderItem width={150}>
+          <Image
+            src="/assets/trustedby/shopify.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={170}>
+          <Image
+            src="/assets/trustedby/coinbase.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={170}>
+          <Image
+            src="/assets/trustedby/rainbow.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={200}>
+          <Image
+            src="/assets/trustedby/walletconnect.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={150}>
+          <Image
+            src="/assets/trustedby/exodus.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={80}>
+          <Image
+            src="/assets/trustedby/picnic.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={140}>
+          <Image
+            src="/assets/trustedby/status.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={120}>
+          <Image
+            src="/assets/trustedby/extra.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={110}>
+          <Image
+            src="/assets/trustedby/stori.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={150}>
+          <Image
+            src="/assets/trustedby/shopify.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={170}>
+          <Image
+            src="/assets/trustedby/coinbase.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={170}>
+          <Image
+            src="/assets/trustedby/rainbow.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={200}>
+          <Image
+            src="/assets/trustedby/walletconnect.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={150}>
+          <Image
+            src="/assets/trustedby/exodus.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={80}>
+          <Image
+            src="/assets/trustedby/picnic.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={140}>
+          <Image
+            src="/assets/trustedby/status.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={120}>
+          <Image
+            src="/assets/trustedby/extra.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={110}>
+          <Image
+            src="/assets/trustedby/stori.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+      </SliderContainer>
+      <SliderContainer className="" contentWidth={1290} initialOffsetX={0}>
+        <SliderItem width={140}>
+          <Image
+            src="/assets/trustedby/status.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={120}>
+          <Image
+            src="/assets/trustedby/extra.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={110}>
+          <Image
+            src="/assets/trustedby/stori.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={150}>
+          <Image
+            src="/assets/trustedby/shopify.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={170}>
+          <Image
+            src="/assets/trustedby/coinbase.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={170}>
+          <Image
+            src="/assets/trustedby/rainbow.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={200}>
+          <Image
+            src="/assets/trustedby/walletconnect.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={150}>
+          <Image
+            src="/assets/trustedby/exodus.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+        <SliderItem width={80}>
+          <Image
+            src="/assets/trustedby/picnic.webp"
+            width={150}
+            height={50}
+            alt="Client Logo"
+            objectFit="contain"
+          />
+        </SliderItem>
+      </SliderContainer>
+    </>
+  )
+}
+
+export default ClientLogos
